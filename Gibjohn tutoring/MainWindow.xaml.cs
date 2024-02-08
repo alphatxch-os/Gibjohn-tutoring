17d5fbdbe714b81da92c67f9f0e4d29af8cde10b
Josh Mathew <S2201761@ndonline.ac.uk>
2024-02-08 10:26:21 +0000

    updated a button onto register window Add project files.

---
+++ Gibjohn tutoring/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
+
+namespace Gibjohn_tutoring
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string connStr = "server=ND-COMPSCI;" +
+                      "username=TL_S2201761;" +
+                      "database=TL_S2201761_gj;" +
+                      "port=3306;" +
+                      "password=Notre021205";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void LoginButton_Click_1(object sender, RoutedEventArgs e)
+        {
+            string username = UsernameTextBox.Text;
+            string password = PasswordBox.Password;
+
+            string connStr = "server=ND-COMPSCI;username=TL_S2201761;database=TL_S2201761_gj;port=3306;password=Notre021205";
+
+            try
+            {
+                using (MySqlConnection conn = new MySqlConnection(connStr))
+                {
+                    conn.Open();
+
+                    string sql = "SELECT * FROM login WHERE username = @username AND password = @password";
+                    MySqlCommand cmd = new MySqlCommand(sql, conn);
+                    cmd.Parameters.AddWithValue("@username", username);
+                    cmd.Parameters.AddWithValue("@password", password);
+
+                    using (MySqlDataReader rdr = cmd.ExecuteReader())
+                    {
+                        if (rdr.Read())
+                        {
+                            MessageBox.Show("Login successful!");
+                            this.Close();
+                            // Proceed to next window or perform any other action
+                        }
+                        else
+                        {
+                            MessageBox.Show("Invalid username or password. Please try again.");
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+    }
+}
+
